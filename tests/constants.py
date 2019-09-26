89d4e880c3c33756260b991ccbb59e5edceeda3c
mtatsuma <tatsuma.matsuki@gmail.com>
2019-09-26 13:12:23 +0900

    Add test code

---
+++ tests/constants.py
@@ -0,0 +1,63 @@
+
+REMO_API_BASE_URL = "https://api.nature.global"
+REMO_DEVICES_URL = REMO_API_BASE_URL + "/1/devices"
+REMO_DEVICES = [
+    {
+        "id": "fbe9dbf5-18cf-492e-bc45-7a8adfbbca33",
+        "name": "remo-1",
+        "temperature_offset": 0,
+        "humidity_offset": 0,
+        "created_at": "2019-09-26T22:52:58.424Z",
+        "updated_at": "2019-09-26T22:52:58.424Z",
+        "firmware_version": "string",
+        "mac_address": "string",
+        "serial_number": "string",
+        "newest_events": {
+            "te": {
+                "value": 26,
+                "created_at": "2019-09-26T22:52:58.424Z"
+            },
+            "hu": {
+                "value": 50,
+                "created_at": "2019-09-26T22:52:58.424Z"
+            },
+            "il": {
+                "value": 150,
+                "created_at": "2019-09-26T22:52:58.424Z"
+            },
+            "mo": {
+                "value": 150,
+                "created_at": "2019-09-26T22:52:58.424Z"
+            }
+        }
+    },
+    {
+        "id": "2c544f59-f82f-4670-8c9d-2c114cf18956",
+        "name": "remo-2",
+        "temperature_offset": 0,
+        "humidity_offset": 0,
+        "created_at": "2019-09-27T22:52:58.424Z",
+        "updated_at": "2019-09-27T22:52:58.424Z",
+        "firmware_version": "string",
+        "mac_address": "string",
+        "serial_number": "string",
+        "newest_events": {
+            "te": {
+                "value": 23,
+                "created_at": "2019-09-27T22:52:58.424Z"
+            },
+            "hu": {
+                "value": 60,
+                "created_at": "2019-09-27T22:52:58.424Z"
+            },
+            "il": {
+                "value": 200,
+                "created_at": "2019-09-27T22:52:58.424Z"
+            },
+            "mo": {
+                "value": 0,
+                "created_at": "2019-09-27T22:52:58.424Z"
+            }
+        }
+    }
+]
