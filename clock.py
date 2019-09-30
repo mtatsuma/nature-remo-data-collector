05197dde1f4b6f445db79a69744c9bbffdb295db
mtatsuma <tatsuma.matsuki@gmail.com>
2019-10-01 08:23:36 +0900

    Add clock.py to run custom scheduled jobs

---
+++ clock.py
@@ -0,0 +1,15 @@
+import os
+from apscheduler.schedulers.blocking import BlockingScheduler
+import worker
+
+
+sched = BlockingScheduler(timezone="UTC")
+
+
+@sched.scheduled_job('interval',
+                     minutes=os.environ.get('REMO_CLOCK_INTERVAL', 1))
+def collect_remo_data():
+    worker.run()
+
+
+sched.start()
